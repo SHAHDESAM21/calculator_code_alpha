666777cf84656e47bf603f0851ac200e702793f2
SHAHDESAM21 <shahdesam21e@gmail.com>
2024-10-05 17:53:14 +0300

    try now

--- js/mine.js
+++ js/mine.js
@@ -3,53 +3,18 @@ function appendToDisplay(value) {
   document.getElementById("display").value += value;
 }
 
+// Function to clear the display
 function clearDisplay() {
   document.getElementById("display").value = "";
 }
 
+// Function to calculate the result
 function calculate() {
-var calc=document.getElementById("display").value;
-var result;
-if (calc.includes("*")) {
-    num1 = Number(calc.slice(0,calc.indexOf("*")));
-    num2 = Number(calc.slice(calc.indexOf("*")+1,calc.length));
-    console.log(num1);
-    console.log(typeof(num1))
-    console.log(num2);
-    result=num1*num2
+  try {
+    let result = eval(document.getElementById("display").value);
     document.getElementById("display").value = result;
-}
-else if (calc.includes("/")) {
-  num1 = Number(calc.slice(0, calc.indexOf("/")));
-  num2 = Number(calc.slice(calc.indexOf("/") + 1, calc.length));
-  console.log(num1);
-  console.log(typeof num1);
-  console.log(num2);
-  result = num1 / num2;
-  document.getElementById("display").value = result;
-} else if (calc.includes("+")) {
-  num1 = Number(calc.slice(0, calc.indexOf("+")));
-  num2 = Number(calc.slice(calc.indexOf("+") + 1, calc.length));
-  console.log(num1);
-  console.log(typeof num1);
-  console.log(num2);
-  result = num1 + num2;
-  document.getElementById("display").value = result;
-} else if (calc.includes("-")) {
-  num1 = Number(calc.slice(0, calc.indexOf("-")));
-  num2 = Number(calc.slice(calc.indexOf("-") + 1, calc.length));
-  console.log(num1);
-  console.log(typeof num1);
-  console.log(num2);
-  result = num1 - num2;
-  document.getElementById("display").value = result;
-} else {
+  } catch (e) {
     document.getElementById("display").value = "Error";
+  }
 }
-//   try {
-//     const result = eval(document.getElementById("display").value);
-//     document.getElementById("display").value = result;
-//   } catch (error) {
-//     document.getElementById("display").value = "Error";
-//   }
-}
+
